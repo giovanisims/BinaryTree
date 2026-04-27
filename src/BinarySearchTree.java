cac488ce24c9b20dd1026828734c4e6b11b3c400
giovanisims <giovani@simoes.pro>
2026-04-27 08:26:29 -0300

    add search and remove methods

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,11 +1,18 @@
 public class BinarySearchTree<T extends Comparable<T>> {
 
     private Node<T> root;
-
     public Node<T> getRoot() { return root; }
 
-    public void Insert(T value) { root = insertRecursive(root, value); }
+    private T findMin(Node<T> node) {
+        T min = node.value;
+        while (node.left != null) {
+            min = node.left.value;
+            node = node.left;
+        }
+        return min;
+    }
 
+    public void Insert(T value) { root = insertRecursive(root, value); }
     private Node<T> insertRecursive(Node<T> current, T value) {
 
         if (current == null) { return new Node<>(value); }
@@ -23,9 +30,55 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return current;
     }
 
+    public void Remove(T value) { root = RemoveRecursive(root, value); }
+    private Node<T> RemoveRecursive(Node<T> current, T value) {
+        if (current == null) {
+            System.out.println("Value " + value + " not found!");
+            return null;
+        }
+
+        int comparison = value.compareTo(current.value);
+
+        if (comparison < 0) {
+            current.left = RemoveRecursive(current.left, value);
+        } else if (comparison > 0) {
+            current.right = RemoveRecursive(current.right, value);
+        } else {
+            if (current.left == null) {
+                return current.right;
+            } else if (current.right == null) {
+                return current.left;
+            }
+
+            current.value = findMin(current.right);
+            current.right = RemoveRecursive(current.right, current.value);
+        }
+
+        return current;
+    }
+
+
+    public Node<T> Search(T value) { return SearchRecursive(root, value); }
+
+    private Node<T> SearchRecursive(Node<T> current, T value) {
+        if (current == null) {
+            System.out.println("Value " + value + " not found!");
+            return null;
+        }
+
+        int comparison = value.compareTo(current.value);
+
+        if (comparison < 0) {
+            return SearchRecursive(current.left, value);
+        } else if (comparison > 0) {
+            return SearchRecursive(current.right, value);
+        } else {
+            return current;
+        }
+    }
+
     @Override
     public String toString() { return root == null ? "Tree is empty" : toStringRecursive(root).trim(); }
-
     private String toStringRecursive(Node<T> current) {
         if (current == null) { return ""; }
 
